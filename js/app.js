bfdb0aaca45bd56e68efb0402ccca1cf0450a631
Uriel <urieldiazt1@gmail.com>
2025-04-14 18:47:27 -0600

    Proyecto del clima consumiendo una API

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// const keyAPI = '905769aa5ca341bfe6854a203c7e245c';
+
+//     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${keyAPI}`;
+
+
+//Variables 
+
